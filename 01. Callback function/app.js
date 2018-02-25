6b4c9ec58f8a51e196a2a5d5efb234bc0dc18e04
KwonJH <hojak99@gmail.com>
2018-02-25 13:16:43 +0900

    01. Callback function 추가
    헷갈려,,

---
+++ 01. Callback function/app.js
@@ -0,0 +1,15 @@
+/**
+ * 20180225. Callback function
+ * 자바스크립트에서는 function을 변수 안에 담을 수 있고, 다른 함수에
+ * 전달할 수도 있다. 또한, 함수에서 만들어지고 반환될 수도 있다.
+ */
+
+
+var plus = function(a, b, callback) {
+    var result = a+b;
+    callback(result);       // 인자로 받은 함수 사용
+}
+
+plus(1, 5, function(res){
+    console.log(res);
+});
