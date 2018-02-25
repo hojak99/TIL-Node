f420669bc66fa6cf24868f0c968d60a4e5bf9f0c
KwonJH <hojak99@gmail.com>
2018-02-25 18:34:41 +0900

    05. Express basic 에서 css 추가

--- 05. Express basic/app.js
+++ 05. Express basic/app.js
@@ -8,6 +8,8 @@ var router = require('./router/main')(app);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+
 
 var server = app.listen(8080, function(){
     console.log("Express server has started on port 8080");
