82877754d0357f221090cc825fe43a87349458a8
KwonJH <hojak99@gmail.com>
2018-03-02 18:03:24 +0900

    03. Arrow function 예제 추가 - 진행 중

---
+++ 03. Arrow function/arrowFunction.js
@@ -0,0 +1,20 @@
+/**
+ * 이번엔 Arrow Function 에 대해서 알아볼 시간이다.
+ * 쉽게 생각하면 이 Arrow function 은 좀 더 간단하고 편하게 함수를 정의할 수 있다.
+ * 
+ * 하지만 this 키워드에 대해서 좀 차이가 있다.
+ */
+
+ // 기존에 존재하던 방법
+var arr = [1,2,3,4,5,6,7].map(function(x) {
+     return x*x;
+});
+
+// Arrow function
+var arrowFunction = [1,2,3,4,5,6,7].map(x => { return x * x })
+
+// 두 출력값은 같다.
+console.log(arr);
+console.log(arrowFunction);     
+
+var t
