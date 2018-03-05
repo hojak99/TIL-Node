035c00ca713a6576c836a3ddadc093fdb2e0952b
KwonJH <hojak99@gmail.com>
2018-03-05 22:44:04 +0900

    09. Socket.io 추가
    더 추가할 예정

---
+++ 09. Socket.io/app.js
@@ -0,0 +1,27 @@
+// Load Packages 
+var express     = require('express');
+var http        = require('http');
+var path        = require('path');
+var io          = require('socket.io');
+var app         = express();
+
+app.use(express.static(path.join(__dirname) + '/public'));
+// Configure routes
+var routes = require('./routes')(app);
+
+
+var server = http.createServer(app).listen(8080, (requset, response) => {
+    console.log('Socket IO server has been stated');
+});
+
+io = io.listen(server);
+io.sockets.on('connection', function(socket) {
+    socket.emit('toclient', {msg : 'welcome!'});
+    socket.on('fromclient', function(data) {
+        // 전체 연결된 소켓들을 대표하는 객체가 io.sockets 이다.
+        // io.sockets.emit('이벤트명', function(data) {});      // 나를 포함한 모든 모든 클라에게 이벤트 보내기
+        socket.broadcast.emit('toclint',data);      // 자신을 제외한 다른 클라이언트에게 보냄
+        socket.emit('toclint', data);
+        console.log('Message from client : '+data.msg);
+    });
+})
---
+++ 09. Socket.io/routes/index.js
@@ -0,0 +1,6 @@
+module.exports = function(app) {
+    
+    app.get('/', function(request, response) {
+        response.end("ㅎㅇ");
+    });
+}
